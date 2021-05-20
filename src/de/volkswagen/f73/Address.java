9d9918a810056d15cc37c67996c7bfb729785315
erikg16 <81220397+erikg16@users.noreply.github.com>
2021-05-20 10:28:15 +0200

    implemented customer and address

--- src/de/volkswagen/f73/Address.java
+++ src/de/volkswagen/f73/Address.java
@@ -5,4 +5,32 @@ public class Address {
     private String houseNr;
     private int zipCode;
     private String City;
+	
+    //Getter & Setter 
+    public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getHouseNr() {
+		return houseNr;
+	}
+	public void setHouseNr(String houseNr) {
+		this.houseNr = houseNr;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+    
+    
 }
