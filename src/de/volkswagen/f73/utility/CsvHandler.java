957dd33ab02755e0b2ebd2e682c974be60ee30ca
Fabian <mail@fabian-duerkop.de>
2021-05-20 13:51:21 +0200

    csvHandler preperations and basic userManagement implementation

--- src/de/volkswagen/f73/utility/CsvHandler.java
+++ src/de/volkswagen/f73/utility/CsvHandler.java
@@ -1,14 +1,20 @@
 package de.volkswagen.f73.utility;
 
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import de.volkswagen.f73.Customer;
+
 public class CsvHandler {
+    static String fileName = "Export.csv";
 	
 	public static void CsvExport() {
-		String fileName = "Export.csv";
 		
 		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName));)
 		{
@@ -18,6 +24,42 @@ public class CsvHandler {
 		} catch (Exception e) {
 			System.out.println("EXCEPTION BEIM EXPORT");
 		}
+	}
+	
+	public static Customer[] loadCustomers() {
+	    BufferedReader reader;
+        int customerCount = 0;
+	    try {
+            reader = new BufferedReader(new FileReader(Paths.get(fileName).toString()));
+            int lines = 0;
+            while (reader.readLine() != null) lines++;
+            customerCount = lines -1;
+            reader.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    
+	    Customer[] customers = new Customer[customerCount];
+	    try {
+            reader = new BufferedReader(new FileReader(Paths.get(fileName).toString()));
+            
+            // line.split(',')[0] -> Vorname
+            // Customer[i] = new Customer(Vorname,....)
+            
+            reader.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	    
+	    return null;
 	}
 	
 	
--- src/de/volkswagen/f73/utility/UserManagement.java
+++ src/de/volkswagen/f73/utility/UserManagement.java
@@ -1,5 +1,33 @@
 package de.volkswagen.f73.utility;
 
-public class UserManagement {
+import de.volkswagen.f73.Customer;
 
+public class UserManagement {
+    
+    private Customer[] customers;
+    
+    public UserManagement() {
+        customers = CsvHandler.loadCustomers();
+    }
+    
+    public boolean save() {
+        // TODO implement save method using CsvHandler
+        //return CsvHandler.saveCustomers(customers);
+        return false;
+    }
+    
+    public Customer getUser(int customerNr) {
+        return find(customerNr);
+    }
+    
+    private Customer find(int customerNr) {
+        Customer retCustomer = null;
+        for(Customer c : customers) {
+            if(c.getCustomerNr() == customerNr) {
+                retCustomer = c;
+            }
+        }
+        return retCustomer;
+    }
+    
 }
