a70616823ada6bf547ab0f3e7b80f62859761ca3
PhilippF73 <84457043+PhilippF73@users.noreply.github.com>
2021-05-21 11:12:25 +0200

    removeProdukt funktion

--- src/de/volkswagen/f73/Receipt.java
+++ src/de/volkswagen/f73/Receipt.java
@@ -36,20 +36,38 @@ public class Receipt {
     }
     
     //----- Methoden -----
-    public void addProductToCart(Product productToAdd, int quantity) {
-        if(quantity < 0) {
+    public void addProductToCart(Product productToAdd, int quantityToAdd) {
+        if(quantityToAdd < 0) {
             return;
         }
         for (int i = 0; i < shoppingCart.length; i++) {
-            
-            if (shoppingCart[i] == null && Storage.getStock(productToAdd) - quantity >= 0) {
+            if (shoppingCart[i] == null && Storage.getStock(productToAdd) - quantityToAdd >= 0) {
                 shoppingCart[i] = new Product(productToAdd);
-                shoppingCart[i].setQuantity(quantity);
+                shoppingCart[i].setQuantity(quantityToAdd);
                 break;
             } else if (shoppingCart[i].equals(productToAdd)) {
-                shoppingCart[i].setQuantity(shoppingCart[i].getQuantity() + quantity);
+                shoppingCart[i].setQuantity(shoppingCart[i].getQuantity() + quantityToAdd);
                 break;
             }
+        }
+        calculateTotalPrice();
+    }
+    
+    public void removeProductFromCart(Product productToRemove, int quantityToRemove) {
+        if(quantityToRemove <= 0) {
+            return;
+        }
+        for (int i = 0; i < shoppingCart.length; i++) {
+            if (shoppingCart[i] != null && shoppingCart[i].equals(productToRemove)) {
+                if (shoppingCart[i].getQuantity() - quantityToRemove > 0) {
+                    shoppingCart[i].setQuantity(quantityToRemove);
+                    break;
+                } else {
+                    shoppingCart[i] = null;
+                    break;
+                }
+            }
+            
         }
         calculateTotalPrice();
     }
